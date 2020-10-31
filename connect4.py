e3d1d82e3d0500cc67ef838aa911644ed7b254ca
j-krl <63491353+j-krl@users.noreply.github.com>
2020-10-31 16:38:36 -0600

    bout to mess with the zero_count stuff which might be a bad idea

--- connect4.py
+++ connect4.py
@@ -2,41 +2,58 @@
 #qpy:2
 #qpy:console
 
+import numpy as np
 import time
 import random
 
-board = []
-plain_board = [] #Same board without piece coloring. Used for win checking.
-whos_turn = 0b10
+turn = 0b10
 winner = False
 draw_counter = 0
-column = 0
-boardheight = 6
-boardwidth = 7
 
-#ANSI Color Globals
-#Should probably do this with a Python library that is more universal as ANSI doesn't work the same on all terminals.
 class colors:
+	"""
+	ANSI Color Globals
+	Should probably do this with a Python library that is more universal as ANSI doesn't work the same on all terminals.
+	"""
 	RESET = "\033[0m"
 	RED = "\033[1;31;40m"
 	YELLOW = "\033[1;33;40m"
 
-#generate empty board
-def generate_board(board, boardheight, boardwidth):
-	label = []
-	for i in range(8):
-		if i < boardheight:
-			board.append(["_"] * boardwidth)
-		elif i < boardwidth:
-			board.append(["^"] * boardwidth)
-		else:
-			for j in range(boardwidth):
-				label.append(str(j+1))
-			board.append(label)
+#def generate_board(HEIGHT, WIDTH):
+#	label = []
+#	board = []
+#	for i in range(8):
+#		if i < HEIGHT:
+#			board.append(["_"] * WIDTH)
+#		elif i < WIDTH:
+#			board.append(["^"] * WIDTH)
+#		else:
+#			for j in range(WIDTH):
+#				label.append(str(j+1))
+#			board.append(label)
+#	return board
 
-def start():
-	print ("Let's play Connect Four!")
+def generate_board()
+	"""
+	Creation of the game board as a 2D NumPy array
+	0 = empty square
+	1 = player 1
+	2 = player 2
+	3 = square that is not a legal move square but exists for computational reason
+	4 = current legal move
+	"""
+	ROWS = 12
+	COLUMNS = 13
+	board = np.zeros((ROWS, COLUMNS), dtype=int)
+	for j in range(COLUMNS):	
+		for i in range(ROWS):
+			if i < 3 or i > 8:
+				board[i, j] = 3
+			elif j < 3 or j > 9:
+				board[i, j] = 3
+	return board
 
+# if you do the conversion here you won't need plain_board and board
 def print_board(board):
 	print("\n")
 	for row in board:
@@ -49,19 +66,19 @@ def toggle(old_turn):
 	print("It is player %s's turn." % new_turn)
 	return new_turn
 
-def mark_board(column):
+def mark_board(board, plain_board, column):
 	row = None
 	for i in range(5, -1, -1):
 		if board[i][column] == "_":
 			row = i
 			break
 		if i == 0:
-			if whos_turn == 0b01: 
+			if turn == 0b01: 
 				print("That column is full!")
-			play()
+			play(board, plain_board)
 			return
 	
-	if whos_turn == 0b01:
+	if turn == 0b01:
 		board[row][column] = colors.RED + "1" + colors.RESET
 		plain_board[row][column] = "1"
 	else:
@@ -69,61 +86,58 @@ def mark_board(column):
 		plain_board[row][column] = "2"
 	print_board(board)
 
-def play():
-	if whos_turn == 0b01:
+def play(board, plain_board):
+	if turn == 0b01:
 		column = int(input("Pick a column (1-7): ")) - 1
-		if column >= 0 and column <= boardwidth:
-			mark_board(column)
+		if column >= 0 and column <= WIDTH:
+			mark_board(board, plain_board, column)
 		else:
 			print("You picked a column outside the board!")
-			play()
+			play(board, plain_board)
 	else:
 		column = random.randint(0,6)
-		mark_board(column)
-
-def ai():
-	
+		mark_board(board, plain_board, column)
 
 def check_winner(board, player):
 	#check horizontal spaces
-	for y in range(boardheight):
-		for x in range(boardwidth - 3):
+	for y in range(HEIGHT):
+		for x in range(WIDTH - 3):
 			if board[y][x] == player and board[y][x+1] == player and board[y][x+2] == player and board[y][x+3] == player:
 				return True
 
 	#check vertical spaces
-	for x in range(boardwidth):
-		for y in range(boardheight - 3):
+	for x in range(WIDTH):
+		for y in range(HEIGHT - 3):
 			if board[y][x] == player and board[y+1][x] == player and board[y+2][x] == player and board[y+3][x] == player:
 				return True
 
 	#check / diagonal spaces
-	for x in range(boardwidth - 3):
-		for y in range(3, boardheight):
+	for x in range(WIDTH - 3):
+		for y in range(3, HEIGHT):
 			if board[y][x] == player and board[y-1][x+1] == player and board[y-2][x+2] == player and board[y-3][x+3] == player:
 				return True
 
 	#check \ diagonal spaces
-	for x in range(boardwidth - 3):
-		for y in range(boardheight - 3):
+	for x in range(WIDTH - 3):
+		for y in range(HEIGHT - 3):
 			if board[y][x] == player and board[y+1][x+1] == player and board[y+2][x+2] == player and board[y+3][x+3] == player:
 				return True
 
 	return False
 	
 start()
-generate_board(board, boardheight, boardwidth)
-generate_board(plain_board, boardheight, boardwidth)
+board = generate_board(HEIGHT, WIDTH)
+plain_board = generate_board(HEIGHT, WIDTH)
 print_board(board)
 
 while winner == False and draw_counter != 42:
-	whos_turn = toggle(whos_turn)
-	play()
+	turn = toggle(turn)
+	play(board, plain_board)
 	draw_counter += 1
-	winner = check_winner(plain_board, str(whos_turn))
+	winner = check_winner(plain_board, str(turn))
 
 if winner == True:
-	print("Player " + str(whos_turn) + " wins!")
+	print("Player " + str(turn) + " wins!")
 
 else:
 	print("The game is a draw")
