3ce25b08b2453910b1c320027e0d3fbaacd6f29b
j-krl <63491353+j-krl@users.noreply.github.com>
2021-03-10 13:32:57 -0700

    js hypothetically finished. Migrating to portfolio for testing

--- connect4.js
+++ connect4.js
@@ -1,89 +1,130 @@
 import ai_move from './conn4ai'
 
-ROWS = 12
-COLS = 13
-VISIBLE_ROWS = 6
-VISIBLE_COLS = 7
-LEGAL_ROWS = [3, 4, 5, 6, 7, 8]
-LEGAL_COLS = [3, 4, 5, 6, 7, 8, 9]
+const ROWS = 12
+const COLS = 13
+const VISIBLE_ROWS = 6
+const VISIBLE_COLS = 7
+const LEGAL_ROWS = [3, 4, 5, 6, 7, 8]
+const LEGAL_COLS = [3, 4, 5, 6, 7, 8, 9]
 
-turn = 0b10
-winner = False
-draw_counter = 0
+let turn = 0b10
+let winner = false
+let draw_counter = 0
 
-passes = 2
-if len(sys.argv) > 1:
-	passes = int(sys.argv[1])
+// TODO: Make passes user editable
+const passes = 2 // greater number increases AI difficulty
 
-def toggle(old_turn):
-	mask = 0b11
-	new_turn = old_turn ^ mask
-	print("It is player %s's turn." % new_turn)
-	return new_turn
+function generate_board() {  
+    /*
+     * Creation of the game board as a 2D NumPy array
+     * 0 = empty square
+     * 1 = player 1
+     * 2 = player 2
+     * 3 = square that is not a legal move square but exists for computational reason
+     */
 
-def mark_board(board, column):
-	row = None
-	for i in reversed(LEGAL_ROWS):
-		if board[i][column] == 0:
-			row = i
-			break
-		if i == 3:
-			if turn == 0b01: 
-				print("That column is full!")
-			play(board)
-			return
-	
-	if turn == 0b01:
-		board[row][column] = 1
-	else:
-		board[row][column] = 2
-	
-	print_board(board)
-	return check_winner(board, [row, column], turn)
+    const board = new Array(6).fill(new Array(7).fill(0)) // Initialize board of 0s
+    
+    for (let j = 0; j < COLS; j++) {  	
+        for (let i = 0; i < ROWS; i++) {  
+            if (i < 3 || i > 8) {  
+                board[i][j] = 3
+            }
+            else if (j < 3 || j > 9) {  
+                board[i][j] = 3
+            }
+        }
+    }
+    return board
+}
 
-def play(board):
-	if turn == 0b01:
-		column = int(input("Pick a column (1-7): ")) + 2
-		if column in LEGAL_COLS:
-			return mark_board(board, column)
-		else:
-			print("You picked a column outside the board!")
-			play(board)
-	else:
-		column = int(ai_move(passes, board, turn))
-		return mark_board(board, column)
 
-def check_winner(board, square, player):
-	# Returns True if a player won the game on the last move
-	ROW = square[0]
-	COL = square[1]
-	piece = 0 # Currently selected piece
+function toggle(old_turn) {  
+    const mask = 0b11
+    const new_turn = (old_turn ^ mask)
+    console.log(`It is player ${new_turn}'s turn.`)
+    return new_turn
+}
 
-	for n in range(4):
-		for	i in range(4):
-			for j in range(4):
+function mark_board(board, column) {  
+    let row = null
+    
+    for (let i = 8; i > 2; i--) {  
+        if !(board[i][column]) {  
+            row = i
+            break
+        }
+        if (i == 3) {  
+            if (turn == 0b01) {  
+                print("That column is full!")
+            }
+            play(board)
+            return
+        }
 
-				if n == 0: piece = board[ROW, COL - i + j] # Horizontal
-				elif n == 1: piece = board[ROW - i + j, COL] # Vertical
-				elif n == 2: piece = board[ROW + i - j, COL - i + j] # Diagonal /
-				elif n == 3: piece = board[ROW - i + j, COL - i + j] # Diagonal \
+        if (turn == 0b01) {  
+            board[row][column] = 1
+        } 
+        else {  
+            board[row][column] = 2
+        }
 
-				if piece != player:
-					break
+    return check_winner(board, [row, column], turn)
+}
 
-				if j == 3:
-					return True
-	
-	return False
+function play(board) {
+    // TODO: Give an option to toggle between Human v Human and Human v AI
+    if (turn == 0b01) {  
+        // TODO: function must receive click from front end 
+        return mark_board(board, column)
+        else {  
+            console.log("You picked a column outside the board!")
+            play(board)
+        }
+    }
+    else {  
+        column = ai_move(passes, board, turn) // TODO: Make sure ai_move is valid for mark_board
+        return mark_board(board, column)
+    }
+}
 
-while winner == False and draw_counter != 42:
-	turn = toggle(turn)
-	winner = play(board)
-	draw_counter += 1
+function check_winner(board, square, player) {  
+    // Returns true if a player won the game on the last move
+    const ROW = square[0]
+    const COL = square[1]
+    let piece = 0 // Currently selected piece
 
-if winner == True:
-	print("Player " + str(turn) + " wins!")
+    for (let n = 0; n < 4; n++) {  
+        for (let i = 0; i < 4; i++) {  
+            for (let j = 0; j < 4; j++) {  
 
-else:
-	print("The game is a draw")
+                if (n == 0) piece = board[ROW][COL - i + j] // Horizontal
+                else if (n == 1) piece = board[ROW - i + j][COL] // Vertical
+                else if (n == 2) piece = board[ROW + i - j][COL - i + j] // Diagonal /
+                else if (n == 3) piece = board[ROW - i + j][COL - i + j] // Diagonal \
+
+                if (piece != player) {  
+                    break
+                }
+                if (j == 3) {  
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+
+while (!winner && draw_counter != 42) {  
+    turn = toggle(turn)
+    winner = play(board)
+    draw_counter += 1
+}
+
+if (winner) {  
+    console.log(`Player ${turn} wins!`)
+}
+else {  
+    console.log("The game is a draw")
+}
 
